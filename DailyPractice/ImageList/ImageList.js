f3f09d9d5385fb95a9839b6938d61fa3cf499513
cdf216 <cdf216@163.com>
2016-06-17 16:36:39 +0800

    图片列表选择完善

--- DailyPractice/ImageList/ImageList.js
+++ DailyPractice/ImageList/ImageList.js
@@ -5,5 +5,45 @@ function showPic(whichpic){
     if (!document.getElementById("placeholder")) return true;
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
-    
+    placeholder.setAttribute("src",source);
+    if (!document.getElementById("description")) return false;
+    if (whichpic.getAttribute("title")) {
+        var text = whichpic.getAttribute("title");
+    } else {
+        var text = "";
+    }
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType == 3) {
+        description.firstChild.nodeValue = text;
+    }
+    return false;
 }
+
+//把onclick时间绑定到id等于“imagegallery”的元素内的各个链接元素上。
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for ( var i=0; i < links.length; i++) {
+        links[i].onclick = function() {
+            return showPic(this);
+        }
+        links[i].onkeypress = links[i].onclick;
+    }
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
