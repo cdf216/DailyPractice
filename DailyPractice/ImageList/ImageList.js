6ccbccf750f733a84c8c22d1dd80bc54acf734d6
cdf216 <cdf216@163.com>
2016-06-17 15:51:17 +0800

    图片列表

---
+++ DailyPractice/ImageList/ImageList.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/6/17.
+ */
+function showPic(whichpic){
+    if (!document.getElementById("placeholder")) return true;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    
+}
