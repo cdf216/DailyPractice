a6301bf4692a3c058cf68f6041e90509a3ad4ce7
cdf216 <cdf216@163.com>
2016-07-01 13:38:04 +0800

    删除无效内容

--- DailyPractice/widgetExample/verifyExample-controller.js
+++ DailyPractice/widgetExample/verifyExample-controller.js
@@ -1,34 +1,6 @@
 /**
  * Created by Administrator on 2016/6/30.
  */
-angular.module("myApp", [])
-.controller('verifyExampleCtrl', ['$scope','$state', '$http', function($scope, $state, $http) {
+function studentController($scope) {
 
-    var vm = $scope.vm = {
-        show_error: false,
-        show_type: 1,
-        user: {}
-    };
-
-    vm.submit = function (basic_form) {
-        vm.show_error = true;
-        basic_form.$setDirty();
-        if(basic_form.$valid){
-            alert("提交成功！");
-        }
-    };
-
-    vm.change_show_type = function (form) {
-        if (vm.show_type == 2) {
-            vm.show_error = true;
-        } else {
-            vm.show_error = false;
-        }
-    }
-    $scope.submit = function (userForm) {
-        userForm.$setDirty();
-        if(userForm.$valid){
-            alert("提交成功！");
-        }
-    };
-}])
+}
