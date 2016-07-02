2777c1c301d0b7f02eb894317c86681875204091
cdf216 <cdf216@163.com>
2016-07-02 10:09:03 +0800

    .

---
+++ DailyPractice/widgetExample/cityData.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Administrator on 2016/7/2.
+ */
+//angular.module("verifyController").constant('CityData', [
+//    {
+//        label: '省1',
+//        citys: [
+//            {
+//                label: '市1',
+//                areas: [
+//                    {
+//                        label: '区1'
+//                    },
+//                    {
+//                        label: '区2'
+//                    },
+//                    {
+//                        label: '区3'
+//                    }
+//                ]
+//            },
+//            {
+//                label: '市2',
+//                areas: [
+//                    {
+//                        label: '区1'
+//                    },
+//                    {
+//                        label: '区2'
+//                    },
+//                    {
+//                        label: '区3'
+//                    }
+//                ]
+//            }
+//        ]
+//    },
+//    {
+//        label: '省2',
+//        citys: [
+//            {
+//                label: '市1',
+//                areas: [
+//                    {
+//                        label: '区1'
+//                    },
+//                    {
+//                        label: '区2'
+//                    }
+//                ]
+//            },
+//            {
+//                label: '市2',
+//                areas: [
+//                    {
+//                        label: '区1'
+//                    },
+//                    {
+//                        label: '区2'
+//                    }
+//                ]
+//            },
+//            {
+//                label: '市3'
+//            }
+//        ]
+//    }
+//]);
--- DailyPractice/widgetExample/verifyExample-controller.js
+++ DailyPractice/widgetExample/verifyExample-controller.js
@@ -1,7 +1,19 @@
 /**
  * Created by Administrator on 2016/6/30.
  */
-function verifyController($scope) {
-//下拉列表选项
+angular.module("app",[]).controller("verifyController", function ($scope) {
 
-}
+    //$scope.selected = {};
+    ////省市区选择
+    //var user = $scope.user = {};
+    //user.provinces = CityData;
+    //// 更换省的时候清空市
+    //$scope.$watch('user.province', function(province) {
+    //    user.city = null;
+    //});
+    //// 更换市的时候清空区
+    //$scope.$watch('user.city', function(city) {
+    //    user.area = null;
+    //});
+
+});
